f2318545df566c253cf2a93b93d70f31251e330c
Zhejian Peng <ZHEJIANPENG@YAHOO.COM>
2020-06-11 00:24:36 -0500

    to device

--- final_paper/train_semantic_probe.py
+++ final_paper/train_semantic_probe.py
@@ -139,8 +139,8 @@ def train(dataset,
         tr_loss = 0.
         nb_tr_examples, nb_tr_steps = 0, 0
         for step,  (X, X_mask, labels) in enumerate(tqdm(train_dataloader, desc="Iteration")):
-            X.to(device)
-            X_mask.to(device)
+            X = X.to(device)
+            X_mask = X_mask.to(device)
             # BERT Encoder
             output = encoder(X, attention_mask = X_mask)
 
